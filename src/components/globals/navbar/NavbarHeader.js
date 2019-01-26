1a54845ac8379a4ba5ae35405702f203d121d2c7
F John De Costa <john@mayerfleur.com>
2019-01-26 20:44:34 +1000

    added logo and hamburger menu icon to navbarheader.js. as per video 14.

--- src/components/globals/navbar/NavbarHeader.js
+++ src/components/globals/navbar/NavbarHeader.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
+import {Link} from 'gatsby'
 
+import logo from '../../../images/logo.svg'
+import {FaAlignRight} from 'react-icons/fa'
+import styled from 'styled-components'
 export default class NavbarHeader extends Component {
 	render() {
+		const {handleNavbar} = this.props
 		return (
-			<div>
-			 navbar header content
-			</div>
+			<HeaderWrapper>
+				<Link to="/" >
+				<img src={logo} alt="company name logo" />
+				</Link>
+			<FaAlignRight className="toggle-icon" onClick={()=> {handleNavbar()}}></FaAlignRight>
+			</HeaderWrapper>
 		)
 	}
 }
 
+const HeaderWrapper = styled.div `
+padding: 0.4rem 1rem;
+display: flex;
+align-items: center;
+justify-content: space-between;
+.toggle-icon{
+	font-size: 1.75rem;
+	color: yellow;
+	cursor: pointer;
+}
+@media (min-width: 768px){
+	.toggle-icon {
+		display: none;
+	}
+}
+`
