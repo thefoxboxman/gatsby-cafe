fcd8b923b847d4fa2bc11146c4eee4f080259f4b
F John De Costa <john@mayerfleur.com>
2019-01-26 19:58:47 +1000

    added NavbarOpen, handleNavbar method. Styled navbar with media query

--- src/components/globals/navbar/Navbar.js
+++ src/components/globals/navbar/Navbar.js
@@ -2,15 +2,34 @@ import React, { Component } from 'react'
 import NavbarHeader from './NavbarHeader'
 import NavbarLinks from './NavbarLinks'
 import NavbarIcons from './NavbarIcons'
+import styled from 'styled-components'
 
 export default class navbar extends Component {
+	//set initial state navbar to false ie closed
+	state={
+		navbarOpen: false
+	}
+	//method to handle navbar action
+	handleNavbar = () => {
+		this.state( () => {
+			return {navbarOpen: !this.state.navbarOpen} 
+		})
+	}
 	render() {
 		return (
-			<nav>
-				<NavbarHeader/>
-				<NavbarLinks/>
+			<NavWrapper>
+				<NavbarHeader handleNavbar={this.handleNavbar}/>
+				<NavbarLinks navbarOpen={this.state.navbarOpen}/>
 				<NavbarIcons/>
-			</nav>
+			</NavWrapper>
 		)
 	}
 }
+
+//style the navbar with media query
+const NavWrapper = styled.nav`
+@media (min-width: 768px){
+	display: flex;
+	align-items: center;
+}
+`
