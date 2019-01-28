d8cd8d4188558de956402a00549b928f0a8e1491
F John De Costa <john@mayerfleur.com>
2019-01-28 16:23:04 +1000

    h in header issue

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 import * as styles from  './styles'
-import {HomeHeader, PageHeader} from './Header'
+import {HomeHeader, PageHeader} from './header'
 import Banner from './Banner'
 
 export { styles, HomeHeader, PageHeader, Banner }
