9cf1020b4fd454df715cc21a4d62aea055707603
F John De Costa <john@mayerfleur.com>
2019-01-29 20:48:19 +1000

    Added css and grid layout to Gallary component.

--- src/components/HomePageComponents/Gallery.js
+++ src/components/HomePageComponents/Gallery.js
@@ -46,11 +46,11 @@ export default function Gallery() {
               </div>
               <div className="item item-2">
                 <Img fluid={img2} />
-                <p className="info">Awesome Pizza</p>
+                <p className="info">Awesome Pork</p>
               </div>
               <div className="item item-3">
                 <Img fluid={img3} />
-                <p className="info">Awesome Pizza</p>
+                <p className="info">Awesome Steak</p>
               </div>
             </GalleryWrapper>
           </Section>
@@ -59,4 +59,46 @@ export default function Gallery() {
 	)
 }
 
-const GalleryWrapper = styled.div``
+const GalleryWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto;
+  grid-row-gap: 1rem;
+  .item {
+    position: relative;
+  }
+  .info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: ${styles.colors.mainYellow};
+    padding: 0.1rem 0.3rem;
+    text-transform: capitalize;
+  }
+  @media (min-width: 576px) {
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 1rem;
+  }
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (min-width: 992px) {
+		/*gatsby generated class use in fluid display
+		found using firefox inspector*/
+		.gatsby-image-wrapper{
+			height:100%;
+		}
+    grid-template-areas:
+      'one one two two'
+      'one one three three';
+    .item-1 {
+      grid-area: one;
+    }
+    .item-2 {
+      grid-area: two;
+    }
+    .item-3 {
+      grid-area: three;
+    }
+    grid-gap: 1rem;
+  }
+`
