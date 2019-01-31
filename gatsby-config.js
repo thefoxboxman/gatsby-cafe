79cba6e9e4e5d3d1dd4248c0c44694b253d47eb0
F John De Costa <john@mayerfleur.com>
2019-01-31 18:00:03 +1000

    updated gatsby-config.js to reflex demo name and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby Cafe`,
+    description: `A demo site developed using Gatsby SSG, GraphQL, GitHub repository, Netlify CDN Hosting and Contentful Headless CMS.`,
     author: `@gatsbyjs`,
   },
   plugins: [
